bbb863774be773987d573adcbff290bdf02a8e62
Noga <noga14@gmail.com>
2018-10-31 18:22:49 +0200

    Logging 'test_device' as a Firebase user property.

--- app/src/main/java/com/frizzl/app/frizzleapp/SplashActivity.java
+++ app/src/main/java/com/frizzl/app/frizzleapp/SplashActivity.java
@@ -4,11 +4,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.ArraySet;
+import android.widget.Toast;
 
 import com.frizzl.app.frizzleapp.intro.OnboardingActivity;
 import com.frizzl.app.frizzleapp.map.MapActivity;
 import com.frizzl.app.frizzleapp.preferences.SaveSharedPreference;
 import com.google.firebase.analytics.FirebaseAnalytics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 public class SplashActivity extends Activity{
     private FirebaseAnalytics mFirebaseAnalytics;
@@ -24,6 +32,12 @@ public class SplashActivity extends Activity{
         // Don't sent analytics in debug
         mFirebaseAnalytics.setAnalyticsCollectionEnabled(!BuildConfig.DEBUG);
 //        mFirebaseAnalytics.setAnalyticsCollectionEnabled(true); // TODO: change before release
+
+        String android_id = Settings.Secure.getString(applicationContext.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        List<String> testDevicesIDs = Arrays.asList("f7124e727cd2c363", "e1ba81bb230e0daf");
+        mFirebaseAnalytics.setUserProperty("test_device", String.valueOf(testDevicesIDs.contains(android_id)));
+
 
         // Check if it is the device's first time here
         boolean firstTime = SaveSharedPreference.getFirstTime(applicationContext);
