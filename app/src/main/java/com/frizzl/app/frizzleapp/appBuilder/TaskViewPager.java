21abea292d3c621bcc28b90a0840ae30b855e732
Noga <noga14@gmail.com>
2018-11-27 15:47:35 +0200

    Task text view should now be adjusted to fit the entire text, according to  the device's settings.

--- app/src/main/java/com/frizzl/app/frizzleapp/appBuilder/TaskViewPager.java
+++ app/src/main/java/com/frizzl/app/frizzleapp/appBuilder/TaskViewPager.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 
 import com.frizzl.app.frizzleapp.SwipeDirection;
 
@@ -67,4 +68,21 @@ public class TaskViewPager extends ViewPager {
         this.enabled = enabled;
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        if (height != 0) {
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        }
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
 }
